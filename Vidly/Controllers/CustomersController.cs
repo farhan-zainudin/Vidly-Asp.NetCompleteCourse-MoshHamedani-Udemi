51ec43aff2c4a1ece127c3988efa47c04844c6c9
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2018-02-09 15:10:59 -0400

    Proyecto Nuevo por error creando el code first migration

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("Customers")]
+        public ActionResult Customers()
+        {
+            var list = GetCustomers();
+            return View(list);
+        }
+        
+        [Route("Customers/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().Single(x => x.Id == id);
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            IEnumerable<Customer> customer = new List<Customer>()
+            {
+                new Customer{ Id = 1, Name = "Pedro Martinez" },
+                new Customer{ Id = 2, Name = "Janna Aquino" },
+                new Customer{ Id = 3, Name = "Lisbeth Naomi" }
+            };
+
+            return customer;
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        [Route("Movies")]
+        public ActionResult Movies()
+        {
+            var list = GetMovies();
+
+            return View(list);
+        }
+
+        [Route("Movies/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().Single(x=> x.Id == id);
+
+            return View(movie);
+        }
+
+
+        // GET: Movies/Random
+        [Route("Movies/Random")]
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+
+            var costumers = new List<Customer>()
+            {
+                new Customer {Name = "Pedro"},
+                new Customer {Name = "Juan"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Costumers = costumers
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Movies/Release/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            IEnumerable<Movie> movies = new List<Movie>()
+            {
+                new Movie{ Id = 1, Name= "Black Panther"},
+                new Movie{ Id = 2, Name= "Avengers Infinity War Part I"}
+            };
+
+            return movies;
+        }
+    }
+}
